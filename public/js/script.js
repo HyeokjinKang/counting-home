904eb5716cafef518ff7f18925a72134072ae8bb
HyeokjinKang <coupy@croissant.tech>
2020-12-11 16:47:31 +0900

    bug fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,11 @@
 const update = () => {
   let d = new Date();
-  let remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((32 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
+  let remain = 0;
+  if(d.getHours() >= 9) {
+    remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((32 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
+  } else {
+    remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((9 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
+  }
   let day = Math.floor(remain / 86400);
   let hour = Math.floor((remain - day * 86400) / 3600);
   let min = Math.floor((remain - day * 86400 - hour * 3600) / 60);
