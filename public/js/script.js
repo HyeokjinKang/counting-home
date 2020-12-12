89c026d716cabc95d5c6a8226c3045434664ff4f
HyeokjinKang <coupy@croissant.tech>
2020-12-12 15:34:31 +0900

    귀가완료 표시 추가

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,25 @@
 const update = () => {
   let d = new Date();
   let remain = 0;
-  if(d.getHours() >= 9) {
-    remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((32 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
+  if(d.getDay <= 5) {
+    if(d.getHours() >= 9) {
+      remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((32 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
+    } else {
+      remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((10 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
+    }
+    let day = Math.floor(remain / 86400);
+    let hour = Math.floor((remain - day * 86400) / 3600);
+    let min = Math.floor((remain - day * 86400 - hour * 3600) / 60);
+    let sec = Math.floor(remain - day * 86400 - hour * 3600 - min * 60);
+    dayText.innerText = day;
+    hourText.innerText = hour;
+    minText.innerText = min;
+    secText.innerText = sec;
   } else {
-    remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((9 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
+    homeNoticeText.textContent = '짜잔';
+    homeRemainText.style.fontWeight = '900';
+    homeRemainText.textContent = '귀가완료!';
   }
-  let day = Math.floor(remain / 86400);
-  let hour = Math.floor((remain - day * 86400) / 3600);
-  let min = Math.floor((remain - day * 86400 - hour * 3600) / 60);
-  let sec = Math.floor(remain - day * 86400 - hour * 3600 - min * 60);
-  dayText.innerText = day;
-  hourText.innerText = hour;
-  minText.innerText = min;
-  secText.innerText = sec;
-  console.log(remain);
 };
 
 setInterval(update, 1000);
