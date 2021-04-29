b0db97d8b3c5d116f7b0deb237822fc7d9df8700
header <tworiver1213@naver.com>
2021-04-29 15:36:56 +0900

    repair

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,14 @@
+const homeDays = ["May 04, 2021 08:00:00", "June 05, 2020 08:00:00", ""];
+const schoolDays = ["May 23, 2021 18:00:00", "June 20, 2020 18:00:00", ""];
+
 const isMobile = () => {
   let userAgent = navigator.userAgent;
-  if (userAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || userAgent.match(/LG|SAMSUNG|Samsung/) != null) {
+  if (
+    userAgent.match(
+      /iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i
+    ) != null ||
+    userAgent.match(/LG|SAMSUNG|Samsung/) != null
+  ) {
     return true;
   } else {
     return false;
@@ -8,34 +16,36 @@ const isMobile = () => {
 };
 
 const update = () => {
-  let d = new Date();
-  let remain = 0;
-  if (d.getDay() <= 5) {
-    if (d.getHours() >= 9) {
-      remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((32 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
-    } else {
-      remain = ((5 - d.getDay()) * 24 * 60 * 60) + ((10 - d.getHours()) * 60 * 60) + ((59 - d.getMinutes()) * 60) + (60 - d.getSeconds());
-    }
+  let now = new Date();
+  let nextHomeday = new Date(homeDays[0]);
+  let nextSchoolday = new Date(schoolDays[0]);
+  if (nextHomeday.getTime() < nextSchoolday.getTime()) {
+    let remain = Math.floor((nextHomeday.getTime() - now.getTime()) / 1000);
+
     let day = Math.floor(remain / 86400);
-    let hour = Math.floor((remain - day * 86400) / 3600);
-    let min = Math.floor((remain - day * 86400 - hour * 3600) / 60);
-    let sec = Math.floor(remain - day * 86400 - hour * 3600 - min * 60);
+    remain -= day * 86400;
+    let hour = Math.floor(remain / 3600);
+    remain -= hour * 3600;
+    let min = Math.floor(remain / 60);
+    remain -= min * 60;
+    let sec = remain;
+
     dayText.innerText = day;
-    hourText.innerText = `${hour}`.padStart(2, '0');
-    minText.innerText = `${min}`.padStart(2, '0');
-    secText.innerText = `${sec}`.padStart(2, '0');
+    hourText.innerText = `${hour}`.padStart(2, "0");
+    minText.innerText = `${min}`.padStart(2, "0");
+    secText.innerText = `${sec}`.padStart(2, "0");
   } else {
-    homeNoticeText.textContent = '짜잔';
-    homeRemainText.style.fontWeight = '900';
-    homeRemainText.textContent = '귀가완료!';
+    homeNoticeText.textContent = "짜잔";
+    homeRemainText.style.fontWeight = "900";
+    homeRemainText.textContent = "귀가완료!";
   }
 };
 
 const initialize = () => {
   if (isMobile()) {
-    mainTitle.style.fontSize = '13vw';
-    homeRemainText.style.fontSize = '9vw';
-    homeNoticeText.style.fontSize = '5vw';
+    mainTitle.style.fontSize = "13vw";
+    homeRemainText.style.fontSize = "9vw";
+    homeNoticeText.style.fontSize = "5vw";
   }
   setInterval(update, 1000);
   setInterval(isDay, 60000);
@@ -44,9 +54,9 @@ const initialize = () => {
 };
 
 const isDay = () => {
-  let d = new Date();
-  let hour = parseInt(d.getHours());
-  if(hour <= 7 || hour >= 19) {
+  let now = new Date();
+  let hour = now.getHours();
+  if (hour <= 7 || hour >= 19) {
     document.body.classList.add("dark-mode");
   } else {
     document.body.classList.remove("dark-mode");
